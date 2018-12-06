c0d80c385e4d9dfe928c04b37f29d4bbe436d873
Austin Gorman <austin.gorman@gmail.com>
2018-12-06 17:07:45 -0600

    added signup component and routed the links

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -7,12 +7,33 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    name: "home",
+    components: {
+        HelloWorld
+    }
+};
 </script>
+
+  <style scoped>
+.signUp {
+    margin-top: 40px;
+}
+input {
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+}
+button {
+    margin-top: 10px;
+    width: 10%;
+    cursor: pointer;
+}
+span {
+    display: block;
+    margin-top: 20px;
+    font-size: 11px;
+}
+</style>
