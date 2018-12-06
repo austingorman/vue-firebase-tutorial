1dcbf58a7338c2052c4730b70b54c7e896d0a5ae
Austin Gorman <austin.gorman@gmail.com>
2018-12-06 07:51:38 -0600

    first commit

---
+++ src/Views/Login.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class ="login">
+        <h3>Sign In</h3>
+        <input type="text" placeholder="Email"><br>
+        <input type="password" placeholder="Password"><br>
+        <button>Connection</button>
+        <p>You don't have an account ? You can create one</p>
