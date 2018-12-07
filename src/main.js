741db7e83fda0fbcc9a34148894f6ffdfaf13938
Austin Gorman <39138509+austingorman@users.noreply.github.com>
2018-12-07 15:39:26 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,18 +3,6 @@ import firebase from "firebase";
 import App from "./App.vue";
 import router from "./router";
 
-Vue.config.productionTip = false;
-
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDNkIfLposBI3zNcV6Gd1OUayypZSzlLLo",
-    authDomain: "vue-firebase-tutorial-f13da.firebaseapp.com",
-    databaseURL: "https://vue-firebase-tutorial-f13da.firebaseio.com",
-    projectId: "vue-firebase-tutorial-f13da",
-    storageBucket: "vue-firebase-tutorial-f13da.appspot.com",
-    messagingSenderId: "554636280335"
-};
-firebase.initializeApp(config);
 
 new Vue({
     router,
